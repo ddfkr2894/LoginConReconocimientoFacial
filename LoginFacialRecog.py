6e1d62b35f4ec3a52d3424988f363195fc9f983d
Carlos Cruz <70996545+ddfkr2894@users.noreply.github.com>
2021-08-08 15:02:22 -0500

    Initial commit

---
+++ LoginFacialRecog.py
@@ -0,0 +1,239 @@
+from tkinter import *
+import os
+import cv2
+from matplotlib import pyplot
+from mtcnn.mtcnn import MTCNN
+# from mtcnn import MTCNN
+import numpy as np
+
+# Lo primero que hacemos es crear una función para la pantalla principal
+def pantalla_principal():
+    global pantalla     #Globalizamos la variable para usarla en otras funciones
+    pantalla = Tk()
+    pantalla.geometry("300x250")    # Asignamos el tamaño de la ventana
+    pantalla.title("Chinga tu madre wey!")  #Nombre de la ventana
+    #Caracteristicas de la ventana
+    Label(text="Login con Reconocimiento Facial", bg="gray", width="300", height="2", font=("Verdana", 13)).pack()
+    # Creamos los botones
+    Label(text = "").pack() # Esto es el espacio entre el titulo y el primer boton
+    Button(text = "Iniciar Sesión", height = "2", width = "30", command = login).pack()
+    Label(text = "").pack() # Esto es el espacio entre el primer boton y el segundo boton
+    Button(text = "Registro", height = "2", width = "30", command = registro).pack()
+    pantalla.mainloop()
+
+# Funcion paraa asignar al boton registro
+def registro():
+    global usuario
+    global contra 
+    global usuario_entrada
+    global contra_entrada
+    global pantalla1
+    
+    pantalla1 = Toplevel(pantalla)  # Esta pantalla es de un nivel superiro a la principal
+    pantalla1.title("Registro")
+    pantalla1.geometry("300x250")   # Asignamos el tamaño de la ventana
+    
+    # Se crean las entradas
+    usuario = StringVar()
+    contra = StringVar()
+
+    Label(pantalla1, text = "Registro facial: debe de asignar un usuario:").pack()
+    # Label(pantalla1, text="").pack()    # Para dejar espacio
+    Label(pantalla1, text = "Registro normal: debe de asignar un usuario y una contraseña:").pack()
+    Label(pantalla1, text="").pack()    # Para dejar espacio
+    Label(pantalla1, text="Usuario *").pack()   #Mostramos en la pantalla 1 el usuario
+    usuario_entrada = Entry(pantalla1, textvariable=usuario)    #Creamos un text variable para que el usuario ingrese la info
+    usuario_entrada.pack()
+    Label(pantalla1, text="Contraseña *").pack()    #Mostramos en la pantalla la contraseña
+    contra_entrada = Entry(pantalla1, textvariable=contra)  #Creamos un text variable para que el usuario ingrese la contra
+    contra_entrada.pack()
+    Label(pantalla1, text="").pack()  #Djeamos un espacio para la creacion del boton
+    Button(pantalla1, text ="Registro Normal", width=15, height=1, command=registrar_usuario).pack()
+    # Y creamos otro botón para el registro facial
+    Label(pantalla1, text="").pack()
+    Button(pantalla1, text ="Registro Facial", width=15, height=1, command=registro_facial).pack()
+    
+def registrar_usuario():
+    usuario_info = usuario.get() #Obtenemos la información almacenada del usuario
+    contra_info = contra.get() #Obtenemos la información almacenada en contra
+
+    archivo = open(usuario_info, "w")   #Abriremos la información en modo de escritura
+    archivo.write(usuario_info + "\n")  #Escribimos la información
+    archivo.write(contra_info)
+    archivo.close()
+
+    #Limpiamos los text variables
+    usuario_entrada.delete(0, END)
+    contra_entrada.delete(0, END)
+
+    #Ahora le diremos al usuario que su registro ha sido exitoso
+    Label(pantalla1, text="Registro Cnvencional Exitoso", fg="green", font=("Calibri", 11)).pack()
+
+# Vamos a capturar el rostro
+def registro_facial():
+    cap = cv2.VideoCapture(0)   # Elegimos la camara con la que vamos a hacer la detección
+    
+    while(True):
+        ret, frame = cap.read()     #Leemos el vídeo
+        cv2.imshow('Registro Facial', frame)    #Mostramos el video en pantalla
+        if cv2.waitKey(1) == 27:    #Cuando oprimimos Escape rome el vídeo
+            break
+    
+    usuario_img = usuario.get()
+    cv2.imwrite(usuario_img+".jpg",frame)   # Guardamos la ultima captura del video como imagen y asignamos el nombre del usuario
+    cap.release()       # Cerramos
+    cv2.destroyAllWindows()
+    
+    usuario_entrada.delete(0, END)  #Limpiamos los text variables
+    contra_entrada.delete(0, END)
+    
+    Label(pantalla1, text="Registro Facial Exitoso", fg= "green", font=("Calibri",11)).pack()
+    
+# Detectamos el rostro y exportamos los pixeles
+    def reg_rostro(img, lista_resultados):
+
+        data = pyplot.imread(img)
+
+        for i in range(len(lista_resultados)):
+            x1, y1, ancho, alto = lista_resultados[i]['box']
+            x2, y2 = x1 + ancho, y1 + alto
+            pyplot.subplot(1, len(lista_resultados), i+1)
+            pyplot.axis('off')
+            cara_reg = data[y1:y2, x1:x2]
+            cara_reg = cv2.resize(cara_reg,(150,200), interpolation=cv2.INTER_CUBIC)  #Guardamos la imagen con un tamaño de 150x200
+            cv2.imwrite(usuario_img+".jpg",cara_reg)
+            pyplot.imshow(data[y1:y2, x1:x2])
+        pyplot.show()
+
+    img= usuario_img+".jpg"
+    pixeles = pyplot.imread(img)
+    detector = MTCNN()
+    caras = detector.detect_faces(pixeles)
+    reg_rostro(img, caras)
+
+def login():
+    global pantalla2
+    global verificacion_usuario
+    global verificacion_contra
+    global usuario_entrada2
+    global contra_entrada2
+
+    pantalla2 = Toplevel(pantalla)
+    pantalla2.title("Login")
+    pantalla2.geometry("300x250")   #Creamos la ventana
+    Label(pantalla2, text="Login facial: Debe de asignar un usuario:").pack()
+    Label(pantalla2, text="Login normal: Debe de asignar un usuario y una contraseña:").pack()
+    Label(pantalla2, text="").pack()    #Dejamos un espacio
+    
+    verificacion_usuario = StringVar()
+    verificacion_contra = StringVar()
+    
+    # Ingresamos los datos
+    Label(pantalla2, text="Usuario *").pack()
+    usuario_entrada2 = Entry(pantalla2, textvariable = verificacion_usuario)
+    usuario_entrada2.pack()
+    Label(pantalla2, text="Contraseña * ").pack()
+    contra_entrada2 = Entry(pantalla2, textvariable = verificacion_contra)
+    contra_entrada2.pack()
+    Label(pantalla2, text="").pack()
+    Button(pantalla2, text="Inicio de Sesion Normal", width=20, height=1,command=verificacion_login).pack()
+    #Boton para login facial
+    Label(pantalla2, text="").pack()
+    Button(pantalla2, text="Inicio de Sesion Facial", width=20, height=1,command=login_facial).pack()
+
+def verificacion_login():
+    log_usuario = verificacion_usuario.get()
+    log_contra = verificacion_contra.get()
+
+    usuario_entrada2.delete(0, END)
+    contra_entrada2.delete(0, END)
+
+    lista_archivos = os.listdir()   #Vamos a importar la lista de archivos con la libreria os
+    if log_usuario in lista_archivos:   #Comparamos los archivos con los que nos interesa
+        archivo2 = open(log_usuario, "r")   #Abrimos el archivo en modo lectura
+        verificacion = archivo2.read().splitlines() #llera las lineas dentro del archivo ignorando el resto
+        if log_contra in verificacion:
+            print("Inicio de Sesion Exitoso")
+            Label(pantalla2, text="Inicio de Sesion Exitoso", fg = "green", font=("Calibri", 11)).pack()
+        else:
+            print("Contraseña incorrecta, Ingresar de nuevo")
+            Label(pantalla2, text="Contraseña incorrecta", fg = "red", font=("Calibri", 11)).pack()
+    else:
+        print("Usuario no encontrado")
+        Label(pantalla2, text="Usuario no encontrado", fg = "red", font=("Calibri", 11)).pack()
+
+# Funcion para el Login Facal
+def login_facial():
+    # Vamos a capturar el rostro
+    cap = cv2.VideoCapture(0)   # Elegimos la camara con la que vamos a hacer la detección
+    
+    while(True):
+        ret, frame = cap.read()     #Leemos el vídeo
+        cv2.imshow('Login Facial', frame)    #Mostramos el video en pantalla
+        if cv2.waitKey(1) == 27:    #Cuando oprimimos Escape rome el vídeo
+            break
+    
+    usuario_login = verificacion_usuario.get()  #Con esta variable vamos a guardar la foto pero con otro nombre para no sobreescribir
+    cv2.imwrite(usuario_login+"LOG.jpg",frame)   # Guardamos la ultima captura del video como imagen y asignamos el nombre del usuario
+    cap.release()       # Cerramos
+    cv2.destroyAllWindows()
+    
+    usuario_entrada2.delete(0, END)  #Limpiamos los text variables
+    contra_entrada2.delete(0, END)
+    
+# Funcion para guardar el rostro
+    def log_rostro(img, lista_resultados):
+        data = pyplot.imread(img)
+        for i in range(len(lista_resultados)):
+            x1, y1, ancho, alto = lista_resultados[i]['box']
+            x2, y2 = x1 + ancho, y1 + alto
+            pyplot.subplot(1, len(lista_resultados), i+1)
+            pyplot.axis('off')
+            cara_reg=data[y1:y2, x1:x2]
+            cara_reg=cv2.resize(cara_reg,(150,200), interpolation=cv2.INTER_CUBIC)  #Guardamos la imagen 150x200
+            cv2.imwrite(usuario_login+"LOG.jpg",cara_reg)
+            return pyplot.imshow(data[y1:y2, x1:x2])
+        pyplot.show()
+
+    # Detectamos el rostro
+    img = usuario_login+"LOG.jpg"
+    pixeles = pyplot.imread(img)
+    detector = MTCNN()
+    caras = detector.detect_faces(pixeles)
+    log_rostro(img, caras)
+
+# Funcion para comparar los rostros
+    def orb_sim(img1, img2):
+        orb = cv2.ORB_create()
+
+        kpa, descr_a = orb.detectAndCompute(img1, None)  # Creamos descriptor 1 y extraemos puntos clave
+        kpb, descr_b = orb.detectAndCompute(img2, None)  # Creamos descriptor 1 y extraemos puntos clave
+
+        comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)   #Creamos comparador de fuerza
+
+        matches = comp.match(descr_a, descr_b)  #Aplicamos el comparador a los descriptores
+
+        regiones_similares = [i for i in matches if i.distance < 70]    #Extraemos las regiones similares con base a los puntos clave
+        if len(matches) == 0:
+            return 0
+        return len(regiones_similares)/len(matches)    #Exportamos el porcentaje de similitud
+
+    # Se importan las imagenes y llamaos a la función de comparación
+    im_archivos = os.listdir()  # Se importa la lista de archivos con la libreria os
+    if usuario_login+".jpg" in im_archivos:     # Comparamos los archivos con el que nos interesa
+        rostro_reg = cv2.imread(usuario_login+".jpg",0)     #Importamos el rostro del registro
+        rostro_log = cv2.imread(usuario_login+"LOG.jpg",0)     #Importamos el rostro de inicio de sesión
+        similitud = orb_sim(rostro_reg, rostro_log)
+        if similitud >= 0.7:
+            Label(pantalla2, text="Inicio de Sesión Exitoso", fg ="green", font = ("Calibri", 11)).pack()
+            print("Bienvenido al sistema usuario: ", usuario_login)
+            print("Compatibilidad con la foto del registro: ", similitud)
+        else:
+            print("Rostro invalido, Vuelva a intentarlo: ")
+            print("Compatibilidad con la foto del registro: ", similitud)
+            Label(pantalla2, text= "Incompatibilidad de rostro", fg="red", font=("Calibri", 11)).pack()
+    else:
+        print("Usuario no encontrado")
+        Label(pantalla2, text= "Usuario no encontrado", fg="red", font=("Calibri", 11)).pack()
+
+pantalla_principal()
---
+++ README.txt
@@ -0,0 +1,88 @@
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+
+-----	Log-In system with Facial Recognition Algorithm   -------
+
+To be able tu run this script you need to set up the lastest Python's version
+
+Also get the latest version of matplotlib and mtcnn, what I did was just to use pip install tensorflow
+
+One ohter thing that took me too long to get fixed was the GPU stuff, I had to install the NVIDIA CUDA 11.0 (original version)
+because the version CUDA 11.0.3 didn't work for me.
+
+
+Here is a list of the versions and libraries I used to develop the project:() must of the libraries were installed once I 
+installed Tensor Flow and Nvidia so I suggest you to install those ones first.
+----------------------------------
+C:\Users\Abraham Cruz>python -V
+Python 3.9.6
+----------------------------------
+C:\Users\Abraham Cruz>pip -V
+pip 21.2.3 from C:\Users\Abraham Cruz\AppData\Local\Programs\Python\Python39\lib\site-packages\pip (python 3.9)
+----------------------------------
+C:\Users\Abraham Cruz>pip list
+Package                        Version
+------------------------------ -------------------
+absl-py                        0.13.0
+astunparse                     1.6.3
+cachetools                     4.2.2
+certifi                        2021.5.30
+charset-normalizer             2.0.3
+colorama                       0.4.4
+configparser                   5.0.2
+crayons                        0.4.0
+cycler                         0.10.0
+flatbuffers                    1.12
+gast                           0.4.0
+google-auth                    1.34.0
+google-auth-oauthlib           0.4.5
+google-pasta                   0.2.0
+grpcio                         1.34.1
+h5py                           3.1.0
+idna                           3.2
+Keras                          2.4.3
+keras-nightly                  2.5.0.dev2021032900
+Keras-Preprocessing            1.1.2
+kiwisolver                     1.3.1
+Markdown                       3.3.4
+matplotlib                     3.4.2
+mtcnn                          0.1.1
+numpy                          1.19.5
+oauthlib                       3.1.1
+opencv-python                  4.5.3.56
+opt-einsum                     3.3.0
+Pillow                         8.3.1
+pip                            21.2.3
+protobuf                       3.17.3
+pyasn1                         0.4.8
+pyasn1-modules                 0.2.8
+pycparser                      2.20
+pyparsing                      2.4.7
+python-dateutil                2.8.2
+PyYAML                         5.4.1
+requests                       2.26.0
+requests-oauthlib              1.3.0
+robotframework                 4.1
+robotframework-pythonlibcore   3.0.0
+robotframework-requests        0.9.1
+robotframework-seleniumlibrary 5.1.3
+robotframework-stringformat    0.1.8
+rsa                            4.7.2
+scipy                          1.7.1
+selenium                       3.141.0
+setuptools                     56.0.0
+six                            1.15.0
+tensorboard                    2.6.0
+tensorboard-data-server        0.6.1
+tensorboard-plugin-wit         1.8.0
+tensorflow                     2.5.0
+tensorflow-estimator           2.5.0
+termcolor                      1.1.0
+tk                             0.1.0
+typing-extensions              3.7.4.3
+urllib3                        1.26.6
+webdriver-manager              3.4.2
+Werkzeug                       2.0.1
+wheel                          0.36.2
+wrapt                          1.12.1
+------------------------------ -------------------
